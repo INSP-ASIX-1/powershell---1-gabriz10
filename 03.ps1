64975addfc830335157956c8ef4b2faa3e1fb5b7
gabriz10 <95307535+gabriz10@users.noreply.github.com>
2022-02-11 20:20:04 +0100

    111

--- 03.ps1
+++ 03.ps1
@@ -1,2 +1,2 @@
 #Crea la carpeta myTest des de PowerShell, al mateix lloc on et trobes
-New-item "C:\\My test" -itemType Directory
+mkdir myTest
--- 05.ps1
+++ 05.ps1
@@ -1,2 +1,2 @@
 #Assigna a la variable $a el Id del procés que té per nom 'bash'
-$a = tasklist /svc /FI "ImageName eq bash"
+$a=tasklist /svc /FI "ImageName eq bash"
